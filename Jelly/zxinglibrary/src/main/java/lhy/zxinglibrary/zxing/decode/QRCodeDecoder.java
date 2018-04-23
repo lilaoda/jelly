191903b32ba0eace1de3b4353dd2362ec1ada84a
liheyu <liheyu999@163.com>
2018-04-23 17:56:51 +0800

    增加图片解析二维码方法

--- Jelly/zxinglibrary/src/main/java/lhy/zxinglibrary/zxing/decode/QRCodeDecoder.java
+++ Jelly/zxinglibrary/src/main/java/lhy/zxinglibrary/zxing/decode/QRCodeDecoder.java
@@ -1,6 +1,9 @@
 package lhy.zxinglibrary.zxing.decode;
 
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.AsyncTask;
 import android.text.TextUtils;
 
@@ -47,6 +50,7 @@ public class QRCodeDecoder {
 
         HINTS.put(DecodeHintType.POSSIBLE_FORMATS, allFormats);
         HINTS.put(DecodeHintType.CHARACTER_SET, "utf-8");
+        HINTS.put(DecodeHintType.TRY_HARDER, Boolean.TRUE);
     }
 
     private QRCodeDecoder() {
@@ -70,7 +74,14 @@ public class QRCodeDecoder {
                     RGBLuminanceSource source = new RGBLuminanceSource(width, height, pixels);
                     Result result = new MultiFormatReader().decode(new BinaryBitmap(new HybridBinarizer(source)), HINTS);
                     return result.getText();
+//                    LuminanceSource source1 = new PlanarYUVLuminanceSource(
+//                            rgb2YUV(bitmap), bitmap.getWidth(),
+//                            bitmap.getHeight(), 0, 0, bitmap.getWidth(), bitmap.getHeight(), false);
+//                    BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(source1));
+//                    Result decode = new MultiFormatReader().decode(binaryBitmap, HINTS);
+//                    return decode.getText();
                 } catch (Exception e) {
+                    e.printStackTrace();
                     return null;
                 }
             }
@@ -88,6 +99,21 @@ public class QRCodeDecoder {
         }.execute();
     }
 
+    public static String decodeQRCodeSync(final Bitmap bitmap) {
+        try {
+            int width = bitmap.getWidth();
+            int height = bitmap.getHeight();
+            int[] pixels = new int[width * height];
+            bitmap.getPixels(pixels, 0, width, 0, 0, width, height);
+            RGBLuminanceSource source = new RGBLuminanceSource(width, height, pixels);
+            Result result = new MultiFormatReader().decode(new BinaryBitmap(new HybridBinarizer(source)), HINTS);
+            return result.getText();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
     public interface Delegate {
         /**
          * 解析二维码成功
@@ -100,5 +126,47 @@ public class QRCodeDecoder {
          * 解析二维码失败
          */
         void onDecodeQRCodeFailure();
+    }
+
+    private static byte[] rgb2YUV(Bitmap bitmap) {
+        int width = bitmap.getWidth();
+        int height = bitmap.getHeight();
+        int[] pixels = new int[width * height];
+        bitmap.getPixels(pixels, 0, width, 0, 0, width, height);
+
+        int len = width * height;
+        byte[] yuv = new byte[len * 3 / 2];
+        int y, u, v;
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                int rgb = pixels[i * width + j] & 0x00FFFFFF;
+
+                int r = rgb & 0xFF;
+                int g = (rgb >> 8) & 0xFF;
+                int b = (rgb >> 16) & 0xFF;
+
+                y = ((66 * r + 129 * g + 25 * b + 128) >> 8) + 16;
+                u = ((-38 * r - 74 * g + 112 * b + 128) >> 8) + 128;
+                v = ((112 * r - 94 * g - 18 * b + 128) >> 8) + 128;
+
+                y = y < 16 ? 16 : (y > 255 ? 255 : y);
+                u = u < 0 ? 0 : (u > 255 ? 255 : u);
+                v = v < 0 ? 0 : (v > 255 ? 255 : v);
+
+                yuv[i * width + j] = (byte) y;
+//                yuv[len + (i >> 1) * width + (j & ~1) + 0] = (byte) u;
+//                yuv[len + (i >> 1) * width + (j & ~1) + 1] = (byte) v;
+            }
+        }
+        return yuv;
+    }
+
+    public static Bitmap drawableToBitmap(Bitmap sourceBitmap) {
+        Canvas canvas = new Canvas(sourceBitmap.copy(sourceBitmap.getConfig(), true));
+        // 白色底色   应对透明图
+        Paint paint = new Paint();
+        paint.setColor(Color.WHITE);
+        canvas.drawBitmap(sourceBitmap, 0, 0, paint);
+        return sourceBitmap;
     }
 }
